5aaa1699a9149b6011ebb745a81190738e51b77f
Igor Chernyshev <chernyshevgr@rambler.ru>
2015-10-28 16:58:57 +0300

    Test maven

---
+++ src/main/java/com/jet/Main.java
@@ -0,0 +1,11 @@
+package com.jet;
+
+import  com.jet.present.*;
+
+public class Main
+{
+      public static void main(String[] args)
+      {
+	Printer.main("Hello World");		
+      }
+}
---
+++ src/main/java/com/jet/present/Printer.java
@@ -0,0 +1,8 @@
+package com.jet.present;
+
+public class Printer
+{
+   public static void main(String str){
+ 	System.out.println(str);
+}
+}
