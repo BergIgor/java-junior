103eedbe18a33ff61b56c3c47f2a0c3e61ea6955
Igor Chernyshev <chernyshevgr@rambler.ru>
2015-10-30 16:22:29 +0300

    Course Project Iteration 02: Logger Library - shouldLogSequentIntegersAsSum

--- src/test/java/com/acme/edu/SysoutCaptureAndAssertionAbility.java
+++ src/test/java/com/acme/edu/SysoutCaptureAndAssertionAbility.java
@@ -19,4 +19,6 @@ public interface SysoutCaptureAndAssertionAbility {
     default void assertSysoutContains(String expected) {
         assertThat(OUT.toString()).contains(expected);
     }
+
+    default void resetOut() {OUT.reset();}
 }
